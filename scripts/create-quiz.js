f3510f5ec1cacb0840a5073bd61102de739cb544
carvares <lucas_tavares@id.uff.br>
2021-04-20 13:35:24 -0300

    modified:   index.html 	modified:   scripts/create-quiz.js 	modified:   styles/style.css

--- scripts/create-quiz.js
+++ scripts/create-quiz.js
@@ -4,3 +4,26 @@ function criarQuizz(){
     novaTela.classList.remove("esconder");
     telaAntiga.classList.add("esconder");
 }
+function criarNiveis() {
+    const novaTela = document.querySelector(".novo-quizz");
+    novaTela.innerHTML = `
+            <h1>Crie suas perguntas</h1>
+            <div class="perguntas">
+            <p>Pergunta 1</p>
+            <input type="text" placeholder="Texto da pergunta">
+            <input type="text" placeholder="Cor de fundo da pergunta">
+            <p>Resposta correta</p>
+            <input type="text" placeholder="Resposta correta">
+            <input type="text" placeholder="URL da imagem">
+            <p>Respostas incorretas</p>
+            <input type="text" placeholder="Resposta incorreta 1">
+            <input type="text" placeholder="URL da imagem 1">
+            <input type="text" placeholder="Resposta incorreta 2">
+            <input type="text" placeholder="URL da imagem 2">
+            <input type="text" placeholder="Resposta incorreta 3">
+            <input type="text" placeholder="URL da imagem 3">
+            </div>
+        <div class="proximaTela">Prosseguir pra criar níveis</div>
+    `
+
+}
