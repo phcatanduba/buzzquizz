ca428b1beaed2b0d366ff6971d62785a4413ab29
phcatanduba <phcatanduba@gmail.com>
2021-04-21 12:16:58 -0300

    estilos das respostas + scrollIntoView

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -35,21 +35,22 @@ function acessarQuiz(event) {
         quizEscolhido.questions[i].answers.sort( () => .5 - Math.random() );
 
         if(quizEscolhido.questions[i].answers.length === 4) {
-            conteudo.innerHTML += ` <div class="quiz">
-            <div class="titulo pergunta1" style="background-color: ${quizEscolhido.questions[i].color}">
-                <span>${quizEscolhido.questions[i].title}</span>
-            </div>
-            <div class="respostas">
-                <div>
-                    <span class="resposta">
-                        <img src=${quizEscolhido.questions[i].answers[0].image}>
-                        <p>${quizEscolhido.questions[i].answers[0].text}</p>
-                    </span>
-                    <span class="resposta">
-                        <img src=${quizEscolhido.questions[i].answers[1].image}>
-                        <p>${quizEscolhido.questions[i].answers[1].text}</p>
-                    </span>
+            conteudo.innerHTML += ` 
+            <div class="quiz" id="pergunta${i}">
+                <div class="titulo" style="background-color: ${quizEscolhido.questions[i].color}">
+                    <span>${quizEscolhido.questions[i].title}</span>
                 </div>
+                <div class="respostas">
+                    <div>
+                        <span class="resposta">
+                            <img src=${quizEscolhido.questions[i].answers[0].image}>
+                            <p>${quizEscolhido.questions[i].answers[0].text}</p>
+                        </span>
+                        <span class="resposta">
+                            <img src=${quizEscolhido.questions[i].answers[1].image}>
+                            <p>${quizEscolhido.questions[i].answers[1].text}</p>
+                        </span>
+                    </div>
                 <div>
                     <span class="resposta">
                         <img src=${quizEscolhido.questions[i].answers[2].image}>
@@ -62,21 +63,22 @@ function acessarQuiz(event) {
                 </div>
             <div>    `
         } else if(quizEscolhido.questions[i].answers.length === 3) {
-            conteudo.innerHTML += ` <div class="quiz">
-            <div class="titulo pergunta1" style="background-color: ${quizEscolhido.questions[i].color}">
-                <span>${quizEscolhido.questions[i].title}</span>
-            </div>
-            <div class="respostas">
-                <div>
-                    <span class="resposta">
-                        <img src=${quizEscolhido.questions[i].answers[0].image}>
-                        <p>${quizEscolhido.questions[i].answers[0].text}</p>
-                    </span>
-                    <span class="resposta">
-                        <img src=${quizEscolhido.questions[i].answers[1].image}>
-                        <p>${quizEscolhido.questions[i].answers[1].text}</p>
-                    </span>
+            conteudo.innerHTML += ` 
+            <div class="quiz" id="pergunta${i}">
+                <div class="titulo" style="background-color: ${quizEscolhido.questions[i].color}">
+                    <span>${quizEscolhido.questions[i].title}</span>
                 </div>
+                <div class="respostas">
+                    <div>
+                        <span class="resposta">
+                            <img src=${quizEscolhido.questions[i].answers[0].image}>
+                            <p>${quizEscolhido.questions[i].answers[0].text}</p>
+                        </span>
+                        <span class="resposta">
+                            <img src=${quizEscolhido.questions[i].answers[1].image}>
+                            <p>${quizEscolhido.questions[i].answers[1].text}</p>
+                        </span>
+                    </div>
                 <div>
                     <span class="resposta">
                         <img src=${quizEscolhido.questions[i].answers[2].image}>
@@ -85,11 +87,12 @@ function acessarQuiz(event) {
                 </div>
             <div>    `
         } else if(quizEscolhido.questions[i].answers.length === 2) {
-            conteudo.innerHTML += ` <div class="quiz">
-            <div class="titulo pergunta1" style="background-color: ${quizEscolhido.questions[i].color}">
-                <span>${quizEscolhido.questions[i].title}</span>
-            </div>
-            <div class="respostas">
+            conteudo.innerHTML += ` 
+            <div class="quiz" id="pergunta${i}">
+                    <div class="titulo" style="background-color: ${quizEscolhido.questions[i].color}">
+                        <span>${quizEscolhido.questions[i].title}</span>
+                    </div>
+                <div class="respostas">
                 <div>
                     <span class="resposta">
                         <img src=${quizEscolhido.questions[i].answers[0].image}>
@@ -103,4 +106,68 @@ function acessarQuiz(event) {
             <div>    `
         }
     }
+
+    chamaSelecionaRespostaCorreta = function(event) {
+        selecionaRespostaCorreta(event, quizEscolhido);
+        const idDaPergunta = event.currentTarget.parentNode.parentNode.parentNode.getAttribute("id");
+        setTimeout(function(){proximaPergunta(idDaPergunta)}, 1000);
+    }
+
+    function proximaPergunta(idDaPerguntaAtual) {
+        let numeroDaPergunta = parseInt(idDaPerguntaAtual[idDaPerguntaAtual.length - 1]);
+        let idDaProximaPergunta = idDaPerguntaAtual.slice(0, idDaPerguntaAtual.length - 1);
+        
+        numeroDaPergunta++;
+        idDaProximaPergunta += numeroDaPergunta;
+
+        document.querySelector(`#${idDaProximaPergunta}`).scrollIntoView(false);
+    };
+
+    const todasAsRespostas = document.querySelectorAll(".resposta");
+    todasAsRespostas.forEach((resposta, index) => {
+        resposta.addEventListener("click", chamaSelecionaRespostaCorreta);
+    });
 };
+
+function selecionaRespostaCorreta(event, quizEscolhido) {
+    const respostasDaPergunta = event.currentTarget.parentNode.parentNode;
+    const arrayDasRespostas = respostasDaPergunta.querySelectorAll(".resposta");
+    const respostaEscolhida = event.currentTarget;
+
+    let respostaCorreta;
+    arrayDasRespostas.forEach(resposta => {
+        respostaCorreta = verificaAResposta(resposta, quizEscolhido);
+
+        if(resposta === respostaEscolhida && respostaCorreta) { 
+            resposta.classList.add("resposta-certa");
+        } else if(resposta === respostaEscolhida && !respostaCorreta) {
+            resposta.classList.add("resposta-errada");
+        } else if(!respostaCorreta){
+            resposta.classList.add("outras-respostas");
+            resposta.classList.add("resposta-errada");
+        } else if(respostaCorreta) {
+            resposta.classList.add("outras-respostas");
+            resposta.classList.add("resposta-certa");
+        }
+    });
+    arrayDasRespostas.forEach(resposta => {
+        resposta.removeEventListener("click", chamaSelecionaRespostaCorreta);
+    });
+};
+
+function verificaAResposta(respostaEscolhida, quizEscolhido) {
+    const textoDaRespostaEscolhida = respostaEscolhida.querySelector("p").innerHTML;
+    let resultado;
+    quizEscolhido.questions.forEach((pergunta, i) => {
+        pergunta.answers.forEach((resposta, j) => {
+            if(resposta.text === textoDaRespostaEscolhida) {
+                resultado = resposta.isCorrectAnswer;
+            }
+        });
+    });
+    return resultado;
+};
+
+function carregarButoes() {
+
+}
